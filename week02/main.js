c8a7065bce9ba8d430421e498951f5fa400b12a4
Abel Wenning <abelw@live.com>
2021-01-16 08:25:27 -0800

    chapter 4 notes and code

--- week02/main.js
+++ week02/main.js
@@ -66,3 +66,58 @@ function initCh3QuizProj() {
 
   alert(`Game over.\nYou scored ${score} point${score !== 1 ? 's' : ''}.`);
 }
+
+/*
+ * Chapter 4: Testing
+ */
+const callMe = () => console.log("called me"); // function expressions are NOT hoisted
+callMe();
+
+const arr = [1,3,12,5,23,18,7];
+arr.sort((a,b) => a < b ? -1 : 1); // pass comparator function
+console.log("sorted arr: ", arr);
+
+const callMeNow = function() {console.log("called me now")}();
+
+/**
+ * Chapter 4: Quiz Ninja Project
+ */
+function initCh4QuizProj() {
+  const quiz = [
+    ["What is Superman's real name?","Clark Kent"],
+    ["What is Wonder Woman's real name?","Diana Prince"],
+    ["What is Batman's real name?","Bruce Wayne"]
+  ];
+
+  function start(quiz) {
+    let score = 0;
+
+    // main game loop
+    for (const [question, answer] of quiz){
+      const response = ask(question);
+      check(response,answer);
+    }
+
+    // end of main game loop
+    gameOver();
+
+    // function declarations
+    function ask(question){
+      return prompt(question);
+    }
+
+    function check(response,answer){
+      if (response === answer){
+        alert('Correct!');
+        score++;
+      } else {
+        alert(`Wrong! The correct answer was ${answer}`);
+      }
+    }
+
+    function gameOver(){
+      alert(`Game over.\nYou scored ${score} point${score !== 1 ? 's' : ''}.`);
+    }
+  }
+  start(quiz);
+}
