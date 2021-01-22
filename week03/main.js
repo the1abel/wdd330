a74b0092fe475d87adb872dbaf0e26428064a8e9
Abel Wenning <abelw@live.com>
2021-01-22 11:46:53 -0800

    chapter 6 consolidate code to eliminate hero.html

--- week03/main.js
+++ week03/main.js
@@ -4,6 +4,9 @@ const quiz = [
   { name: "Batman", realName: "Bruce Wayne" }
 ];
 
+/**
+ * Chapter 5: Quiz Ninja Project
+ */
 function initCh5QuizProj() {
   const game = {
     start(quiz) {
@@ -45,6 +48,90 @@ function initCh5QuizProj() {
   game.start(quiz);
 }
 
+/*
+ * Chapter 6: Testing
+ */
+const heroNameLabel = document.getElementById("heroNameLabel");
+console.log("heroNameLabel.htmlFor:", heroNameLabel.htmlFor);
+
+const bats = document.getElementById("bats");
+console.log("bats.classList:", bats.classList);
+bats.classList.replace("vigilante", "loner");
+console.log("bats.classList after replace():", bats.classList);
+bats.classList.toggle("hero");
+console.log("bats.classList after toggle('hero'):", bats.classList);
+bats.classList.toggle("hero");
+console.log("bats.classList after toggle('hero'):", bats.classList);
+
+const roster = document.getElementById("roster");
+
+const buttons = ` <button onclick="moveUp(event)">▲</button>
+                  <button onclick="moveDown(event)">▼</button>
+                  <button onclick="deleteMe(event)">&times;</button>`;
+
+for (const elem of roster.children) {
+  elem.insertAdjacentHTML("beforeend", buttons);
+}
+
+function moveDown(event) {
+  const subjectLi = event.target.parentElement;
+  const ul = subjectLi.parentElement;
+  const allLis = Array.from(ul.children);
+  if (
+    allLis[allLis.length - 1] !== subjectLi &&
+    allLis[allLis.length - 2] !== subjectLi
+  ) {
+    const index = allLis.indexOf(subjectLi);
+    ul.insertBefore(subjectLi, allLis[index + 2]);
+  } else {
+    ul.appendChild(subjectLi);
+  }
+}
+
+function moveUp(event) {
+  const subjectLi = event.target.parentElement;
+  const ul = subjectLi.parentElement;
+  const allLis = Array.from(ul.children);
+  const index = allLis.indexOf(subjectLi);
+  if (index !== 0) {
+    ul.insertBefore(subjectLi, allLis[index - 1]);
+  }
+}
+
+function deleteMe(event) {
+  event.target.parentElement.parentElement.removeChild(event.target.parentElement);
+}
+
+function replaceFirstWithThird() {
+  roster.replaceChild(roster.children[2], roster.children[0]);
+}
+
+function insertHtml() {
+  document.getElementById("target").innerHTML =
+      document.getElementById("heroInput").value;
+}
+
+function addScriptTag() {
+  // abort if script and button were already added
+  if (document.getElementById("testAddedScriptBtn")) {
+    return;
+  }
+
+  const script = document.createElement("script");
+  script.innerText = `function testInsertedScriptTag() {alert("Inserted script tag's function works!")}`;
+  document.body.appendChild(script);
+  
+  const button = document.createElement("button");
+  button.id = "testAddedScriptBtn";
+  button.innerText = "Test inserted script";
+  button.addEventListener("click", testInsertedScriptTag);
+  document.getElementById("heroTesting").appendChild(button);
+}
+
+
+/**
+ * Chapter 6: Quiz Ninja Project
+ */
 function initCh6QuizProj() {
   // View Object
   const view = {
